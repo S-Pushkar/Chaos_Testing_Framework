379ecf18848fd62084de41a077da300f8d180cd5
Pushkar S <spushkar0071234@gmail.com>
2025-01-15 22:29:10 +0530

    Added Hello endpoint

---
+++ com.chaos-testing-framework/src/main/java/master/com/chaos_testing_framework/controller/Hello.java
@@ -0,0 +1,16 @@
+package master.com.chaos_testing_framework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
