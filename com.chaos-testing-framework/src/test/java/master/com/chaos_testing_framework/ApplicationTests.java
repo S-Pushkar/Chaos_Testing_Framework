f2829944d2ca9e344eb28ed54dcbb4f1414c7bf0
Pushkar S <spushkar0071234@gmail.com>
2025-01-15 19:50:07 +0530

    Made a git repo

---
+++ com.chaos-testing-framework/src/test/java/master/com/chaos_testing_framework/ApplicationTests.java
@@ -0,0 +1,13 @@
+package master.com.chaos_testing_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
