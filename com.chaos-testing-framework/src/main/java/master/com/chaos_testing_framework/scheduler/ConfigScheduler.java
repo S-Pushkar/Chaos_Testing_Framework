d6c09be5f63f1641803c7915f3ddf1b77f614b76
RISHITH125 <rishithp125@gmail.com>
2025-04-08 10:22:43 +0530

    getting memory metrics

--- com.chaos-testing-framework/src/main/java/master/com/chaos_testing_framework/scheduler/ConfigScheduler.java
+++ com.chaos-testing-framework/src/main/java/master/com/chaos_testing_framework/scheduler/ConfigScheduler.java
@@ -1,11 +1,18 @@
 package master.com.chaos_testing_framework.scheduler;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.Statistics;
+import master.com.chaos_testing_framework.service.PrometheusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -22,16 +29,20 @@ import master.com.chaos_testing_framework.service.ExecuteService;
 @Slf4j
 @Service
 public class ConfigScheduler {
+    PrometheusService prometheusService;
+
     ConfigAndMetadataManager configAndMetadataManager;
     ExecuteService executeService;
-
+    DockerClient dockerClient;
     @Value("${chaos_testing_framework.probability_of_rolling_back_random_container:0.25}")
     private double probabilityOfRollingBackRandomContainer;
 
     @Autowired
-    public ConfigScheduler(ConfigAndMetadataManager configAndMetadataManager, ExecuteService executeService) {
+    public ConfigScheduler(ConfigAndMetadataManager configAndMetadataManager, ExecuteService executeService, DockerClient dockerClient,PrometheusService prometheusService) {
         this.configAndMetadataManager = configAndMetadataManager;
         this.executeService = executeService;
+        this.dockerClient = dockerClient;
+        this.prometheusService = prometheusService;
     }
 
     @Scheduled(fixedDelayString = "2500")
@@ -67,4 +78,49 @@ public class ConfigScheduler {
             configAndMetadataManager.updateConfigsForChaosTest(element.getKey(), services, newTimeStamp);
         }
     }
+
+    @Scheduled(fixedDelayString = "1000")
+    public void mayhemWatcher(){
+        for (Map.Entry<String, ConfigManagerValue> element : configAndMetadataManager.getConfigs().entrySet()){
+            List<MicroService> services = element.getValue().microService();
+            for (MicroService microService : services) {
+                String containerName = microService.getContainerName();
+                InspectContainerResponse containerResponse = dockerClient.inspectContainerCmd(containerName).exec();
+                Long currentRAM = containerResponse.getHostConfig().getMemory();
+                System.out.println(currentRAM);
+                prometheusService.updateMemory(containerName, currentRAM);
+//                dockerClient.statsCmd(containerName).exec(new ResultCallback<Statistics>() {
+//                    @Override
+//                    public void onStart(Closeable closeable) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(Statistics object) {
+////                        System.out.println(object.getMemoryStats().getUsage());
+//                        long memory = object.getMemoryStats().getUsage();
+//                         prometheusService.updateMemory(containerName, memory);
+//
+//                    }
+//
+//
+//                    @Override
+//                    public void onError(Throwable throwable) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
+//
+//                    @Override
+//                    public void close() throws IOException {
+//
+//                    }
+//                });
+            }
+        }
+    }
+
 }
